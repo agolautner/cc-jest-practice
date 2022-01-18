e40693896495e312325d1027864d9f0abde8f6cd
agolautner <lautner.agoston@gmail.com>
2022-01-18 22:40:54 +0100

    more test cases added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const preschoolAdd = (num1, num2) => {
     let number = num1 + num2;
     if (number > 10) return 'sok';
-    if (number === 0) return 0;
+    if (number < 0) return 0;
     
     return Math.floor(number);
 }
@@ -10,3 +10,4 @@ console.log('hello world');
 console.log(preschoolAdd(1,2)); // should return 3
 console.log(preschoolAdd(1,10)); // should return 'sok'
 console.log(preschoolAdd(5,3.8)); // should return 8
+console.log(preschoolAdd(5,-9)); // should return 0
