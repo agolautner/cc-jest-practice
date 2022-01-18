a5d0e50f6b35dc71336990c4e67a4ea7aeec487c
agolautner <lautner.agoston@gmail.com>
2022-01-18 23:24:10 +0100

    testing function refactored

--- test-preschoolAdd.js
+++ test-preschoolAdd.js
@@ -2,18 +2,19 @@
 const preschoolAdd = require('./preschoolAdd');
 
 const check = (result, expected, testCaseDescription) => {
-    console.log(testCaseDescription);
-    if (result === expected) {
-        console.log('SUCCESS');
-    } else {
-        console.log('FAILED');
-    }
+    // console.log(testCaseDescription);
+    // if (result === expected) {
+    //     console.log('SUCCESS');
+    // } else {
+    //     console.log('FAILED');
+    // }
+    console.log(`${result === expected ? 'SUCCESS' : 'FAILED'} :: Result: ${result} :: Expected: ${expected} :: ${testCaseDescription}`);
 }
 
-check(preschoolAdd(2,4),6,'two integers under 10, expecting 6');
-check(preschoolAdd(1,10),'sok','result over 10, expecting the string SOK');
-check(preschoolAdd(5,3.8),8,'result is fractional, expecting it rounded to 8');
-check(preschoolAdd(5,-9),0,'result lower than 0, expecting 0');
+check(preschoolAdd(2,4),6,'Two integers under 10, expecting 6');
+check(preschoolAdd(1,10),'sok','Result over 10, expecting the string SOK');
+check(preschoolAdd(5,3.8),8,'Result is fractional, expecting it rounded to 8');
+check(preschoolAdd(5,-9),0,'Result lower than 0, expecting 0');
 
 /* console.log('hello world');
 console.log(preschoolAdd(1,2)); // should return 3
