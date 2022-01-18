d335f96570636e8fc4b7fe26d4179e9c40ae5cea
agolautner <lautner.agoston@gmail.com>
2022-01-18 22:31:15 +0100

    shortened the function a bit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const preschoolAdd = (num1, num2) => {
     let number = num1 + num2;
     if (number > 10) return 'sok';
     if (number === 0) return 0;
-    if (!Number.isInteger(number)) return Math.floor(number);
-    return number;
+    
+    return Math.floor(number);
 }
 
 console.log('hello world');
